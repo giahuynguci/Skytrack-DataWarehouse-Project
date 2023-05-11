c57a2973d889c502977b3e3f229e698f3212b979
ADMIN <huytg0703@gmail.com>
2023-05-12 00:08:47 +0700

    Create BULK INSERT

---
+++ Skytrack-DataWarehouse-Project/Schema/BULK_Insert_To_stgAirport.sql
@@ -0,0 +1,9 @@
+﻿BULK INSERT [stg].[staging_airport]
+FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\airport\airport.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	DATAFILETYPE    = 'char', 
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR   = '0x0D0A'
+);

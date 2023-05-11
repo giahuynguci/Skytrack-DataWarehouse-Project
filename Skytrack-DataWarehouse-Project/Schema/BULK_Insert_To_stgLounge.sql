2529760dd77469efa2a4bb62f181b8b8d65af99e
ADMIN <huytg0703@gmail.com>
2023-05-12 00:20:47 +0700

    Repaire BULK Insert

--- Skytrack-DataWarehouse-Project/Schema/BULK_Insert_To_stgLounge.sql
+++
@@ -1,9 +0,0 @@
-﻿BULK INSERT [stg].[staging_lounge]
-FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\lounge\lounge.csv'
-WITH (
-	FORMAT = 'CSV',
-	FIRSTROW = 2,
-	DATAFILETYPE    = 'char', 
-	FIELDTERMINATOR = ',', 
-	ROWTERMINATOR   = '0x0D0A'
-);

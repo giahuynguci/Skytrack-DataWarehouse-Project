d02498c9d952ae25a683fdbd28721e82b0ee275f
ADMIN <huytg0703@gmail.com>
2023-05-12 00:20:47 +0700

    Repaire BULK Insert

---
+++ Skytrack-DataWarehouse-Project/Schema/BULK_Insert_To_Stg.sql
@@ -0,0 +1,39 @@
+﻿BULK INSERT [stg].[staging_airline]
+FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\airline_full\airline.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	DATAFILETYPE    = 'char', 
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR   = '0x0D0A'
+);
+----------------------------------------------------------
+BULK INSERT [stg].[staging_airport]
+FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\airport\airport.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	DATAFILETYPE    = 'char', 
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR   = '0x0D0A'
+);
+----------------------------------------------------------
+BULK INSERT [stg].[staging_lounge]
+FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\lounge\lounge.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	DATAFILETYPE    = 'char', 
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR   = '0x0D0A'
+);
+----------------------------------------------------------
+BULK INSERT [stg].[staging_seat]
+FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\seat\seat.csv'
+WITH (
+	FORMAT = 'CSV',
+	FIRSTROW = 2,
+	DATAFILETYPE    = 'char', 
+	FIELDTERMINATOR = ',', 
+	ROWTERMINATOR   = '0x0D0A'
+);
--- Skytrack-DataWarehouse-Project/Schema/BULK_Insert_To_StgSeat.sql
+++
@@ -1,9 +0,0 @@
-﻿BULK INSERT [stg].[staging_seat]
-FROM 'C:\Users\ADMIN\Desktop\Skytrack Dataset\seat\seat.csv'
-WITH (
-	FORMAT = 'CSV',
-	FIRSTROW = 2,
-	DATAFILETYPE    = 'char', 
-	FIELDTERMINATOR = ',', 
-	ROWTERMINATOR   = '0x0D0A'
-);

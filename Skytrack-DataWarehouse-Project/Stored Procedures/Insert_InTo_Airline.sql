35a180f43ce5ad5433815cd52de2d319191213d3
ADMIN <huytg0703@gmail.com>
2023-06-20 10:04:05 +0700

    add some comment line

--- Skytrack-DataWarehouse-Project/Stored Procedures/Insert_InTo_Airline.sql
+++ Skytrack-DataWarehouse-Project/Stored Procedures/Insert_InTo_Airline.sql
@@ -1,18 +1,27 @@
 ﻿CREATE PROCEDURE [dwh].[Insert_To_Airline]
-
 AS
 	SET NOCOUNT ON;
 
+	-- Inserting data into dim_airlines table
     INSERT INTO [dwh].[dim_airlines] ([airline_name], [cabin_flown], [Link], [route])
+
+	-- Selecting airline_name, cabin_flown, link, and route from staging tables
 	SELECT [airline_name], [cabin_flown], [link], [route]
 	FROM (
+		-- Selecting distinct airline_name, cabin_flown, link, and route from staging_airline table
 		SELECT DISTINCT [airline_name], [cabin_flown], [link], [route]
 		FROM [stg].[staging_airline]
+		
+		-- Union with distinct airline_name, null values for cabin_flown and route, and link from staging_lounge table
 		UNION
-		SELECT DISTINCT [airline_name], NULL as [cabin_flown], [link], NULL as [route]
+		SELECT DISTINCT [airline_name], NULL AS [cabin_flown], [link], NULL AS [route]
 		FROM [stg].[staging_lounge]
+		
+		-- Union with distinct airline_name, cabin_flown, null value for route, and link from staging_seat table
 		UNION
-		SELECT DISTINCT [airline_name], [cabin_flown], [link], NULL as [route]
+		SELECT DISTINCT [airline_name], [cabin_flown], [link], NULL AS [route]
 		FROM [stg].[staging_seat]
 	) AS dim_airlines
-RETURN 0
+
+	-- Return 0 to indicate successful execution
+	RETURN 0
--- Skytrack-DataWarehouse-Project/Stored Procedures/Insert_Into_Airport.sql
+++ Skytrack-DataWarehouse-Project/Stored Procedures/Insert_Into_Airport.sql
@@ -1,14 +1,22 @@
 ﻿CREATE PROCEDURE [dwh].[Insert_Into_Airport]
-
 AS
 	SET NOCOUNT ON;
 
+	-- Inserting data into dim_airports table
     INSERT INTO [dwh].[dim_airports] ([airport_name], [link], [experience_airport])
-    SELECT DISTINCT [airport_name],[link],[experience_airport]
+	
+	-- Selecting distinct airport_name, link, and experience_airport from staging tables
+    SELECT DISTINCT [airport_name], [link], [experience_airport]
 	FROM (
-	SELECT DISTINCT [airport_name],[link],[experience_airport]
-	FROM [stg].[staging_airport]
-	UNION 
-	SELECT DISTINCT [airport],[link],NULL as [experience_airport]
-	FROM [stg].[staging_lounge]) as dim_airport
-RETURN 0
+		-- Selecting distinct airport_name, link, and experience_airport from staging_airport table
+		SELECT DISTINCT [airport_name], [link], [experience_airport]
+		FROM [stg].[staging_airport]
+		
+		-- Union with distinct airport, link, and null value for experience_airport from staging_lounge table
+		UNION 
+		SELECT DISTINCT [airport], [link], NULL AS [experience_airport]
+		FROM [stg].[staging_lounge]
+	) AS dim_airport
+
+	-- Return 0 to indicate successful execution
+	RETURN 0
